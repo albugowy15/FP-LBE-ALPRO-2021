a75d77bb9985fad79354a936dec5f54ef9c48c15
albugowy15 <muhammadbughowy@gmail.com>
2021-09-18 13:52:32 +0700

    Add Light and Dark Mode

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+var icon = document.getElementById("fab");
+var bg = document.getElementById("top");
+icon.onclick = function() {
+    document.body.classList.toggle("light-theme");
+    if(document.body.classList.contains("light-theme")) {
+        bg.style.backgroundImage = "url('bg-2.jpg')";
+    } else {
+        bg.style.backgroundImage = "url('bg-1.jpg')";
+    }
+}
+
 function _scrollTo(curr, id) {
     let element = document.getElementById(id);
     let subtractor = 70; // getAdder(id)
